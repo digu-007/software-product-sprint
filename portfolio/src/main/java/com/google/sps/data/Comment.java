cebb7bdd4b2efb6c37c67b12bfff9242ee2abe88
Digvijay <jasonryder555@gmail.com>
2020-07-19 21:39:33 +0530

    Update POJO with Lombok to reduce code

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -1,17 +1,13 @@
 package com.google.sps.data;
 
+import lombok.AllArgsConstructor;
+
 /** An item on the comment list. */
+@AllArgsConstructor
 public final class Comment {
 
     private final long id;
     private final String data;
     private final String userEmail;
     private final long timestamp;
-
-    public Comment(long id, String data, String userEmail, long timestamp) {
-        this.id = id;
-        this.data = data;
-        this.userEmail = userEmail;
-        this.timestamp = timestamp;
-    }
 }
